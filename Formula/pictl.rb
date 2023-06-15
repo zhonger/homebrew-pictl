023d9d05ebbd941b1c6fd514b79fe5f1a5f2545b
zhonger <zhonger@live.cn>
2023-06-15 12:48:12 +0900

    Update python version

--- Formula/pictl.rb
+++ Formula/pictl.rb
@@ -11,7 +11,7 @@ class Pictl < Formula
         url :stable
     end
     
-    depends_on "python@3.10"
+    depends_on "python@3.10.9"
     
     resource "blessed" do
         url "https://files.pythonhosted.org/packages/25/ae/92e9968ad23205389ec6bd82e2d4fca3817f1cdef34e10aa8d529ef8b1d7/blessed-1.20.0.tar.gz"
